86e16183147e86345f175e35a6e2b053fc9ad3b0
Federico-Zorzi <federico.zorzi05@gmail.com>
2024-12-04 17:32:50 +0100

    starting with tags section - to develop

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -13,6 +13,7 @@ export default function Main() {
     image: "",
     category: "",
     isPublished: false,
+    tags: [""],
   };
 
   const [formData, setformData] = useState(defaultArticle);
@@ -202,6 +203,59 @@ export default function Main() {
                   </div>
                 </div>
               </div>
+
+              {/* TO DEVELOP - INPUT FOR ARTICLE TAGS */}
+              {/*               <div className="col">
+                <label htmlFor="articleTags" className="form-label">
+                  Tags
+                </label>
+
+                <div
+                  id="articleTags"
+                  className="btn-group"
+                  role="group"
+                  aria-label="Basic checkbox toggle button group"
+                >
+                  <input
+                    type="checkbox"
+                    className="btn-check"
+                    id="btncheck1"
+                    autoComplete="off"
+                  />
+                  <label
+                    className="btn btn-outline-primary"
+                    htmlFor="btncheck1"
+                  >
+                    Checkbox 1
+                  </label>
+
+                  <input
+                    type="checkbox"
+                    className="btn-check"
+                    id="btncheck2"
+                    autoComplete="off"
+                  />
+                  <label
+                    className="btn btn-outline-primary"
+                    htmlFor="btncheck2"
+                  >
+                    Checkbox 2
+                  </label>
+
+                  <input
+                    type="checkbox"
+                    className="btn-check"
+                    id="btncheck3"
+                    autoComplete="off"
+                  />
+                  <label
+                    className="btn btn-outline-primary"
+                    htmlFor="btncheck3"
+                  >
+                    Checkbox 3
+                  </label>
+                </div>
+              </div> */}
             </div>
 
             {/* BUTTON FOR SUBMIT */}
